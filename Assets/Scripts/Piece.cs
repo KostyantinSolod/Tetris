933cb1888c7f158284de0919a1cb013e3976381a
KostyantinSolod <76468012+KostyantinSolod@users.noreply.github.com>
2022-11-22 18:07:38 +0200

    Realize

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -6,7 +6,7 @@ public class Piece : MonoBehaviour
 {
     public Board board { get; private set; }
     public TetrominoData data { get; private set; }
-    public Vector3Int[] cells { get; private set; } 
+    public Vector3Int[] cells { get; private set; }
     public Vector3Int position { get; private set; }
     public int rotationIndex { get; private set; }
     public float stepDelay = 1f;
@@ -15,9 +15,16 @@ public class Piece : MonoBehaviour
     private float stepTime;
     private float moveTime;
     private float lockTime;
-    public void Initialize(Board board,Vector3Int position, TetrominoData data)
+
+#if UNITY_ANDROID
+    Vector2 firstPressPos;
+    Vector2 secondPressPos;
+    Vector2 currentSwipe;
+#endif
+
+    public void Initialize(Board board, Vector3Int position, TetrominoData data)
     {
-        this.position = position; 
+        this.position = position;
         this.data = data;
         this.board = board;
         rotationIndex = 0;
@@ -28,15 +35,18 @@ public class Piece : MonoBehaviour
         {
             this.cells = new Vector3Int[data.cells.Length];
         }
+
         for (int i = 0; i < data.cells.Length; i++)
         {
             this.cells[i] = (Vector3Int)data.cells[i];
         }
     }
+
     void Update()
     {
         this.board.Clear(this);
         this.lockTime += Time.deltaTime;
+#if UNITY_EDITOR
         if (Input.GetKeyDown(KeyCode.Q))
         {
             Rotate(-1);
@@ -45,21 +55,73 @@ public class Piece : MonoBehaviour
         {
             Rotate(1);
         }
+
         if (Time.time > moveTime)
         {
             HandleMoveInputs();
         }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             HardDrop();
         }
+#endif
+
+#if UNITY_ANDROID
+        if (Time.time > moveTime)
+            AndroidInput();
+#endif
+
         if (Time.time >= this.stepTime)
         {
             Step();
         }
+
+
         this.board.Set(this);
     }
-    private void HandleMoveInputs()
+
+    private void AndroidInput()
+    {
+        if (Input.touches.Length > 0)
+        {
+            Touch t = Input.GetTouch(0);
+            if (t.phase == TouchPhase.Began)
+            {
+                firstPressPos = new Vector2(t.position.x, t.position.y);
+            }
+
+            if (t.phase == TouchPhase.Ended)
+            {
+                secondPressPos = new Vector2(t.position.x, t.position.y);
+                if (firstPressPos == secondPressPos)
+                {
+                    Rotate(1);
+                }
+                
+                currentSwipe = new Vector3(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+                
+                currentSwipe.Normalize();
+                
+                if (currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+                {
+                    HardDrop();
+                }
+                
+                if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+                {
+                    Move(Vector2Int.left);
+                }
+                
+                if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+                {
+                    Move(Vector2Int.right);
+                }
+            }
+        }
+    }
+
+    private void  HandleMoveInputs()
     {
         if (Input.GetKey(KeyCode.S))
         {
@@ -68,6 +130,7 @@ public class Piece : MonoBehaviour
                 stepTime = Time.time + stepDelay;
             }
         }
+
         if (Input.GetKey(KeyCode.A))
         {
             Move(Vector2Int.left);
@@ -77,43 +140,50 @@ public class Piece : MonoBehaviour
             Move(Vector2Int.right);
         }
     }
+
     private void Step()
     {
         this.stepTime = Time.time + this.stepDelay;
         Move(Vector2Int.down);
-        if (this.lockTime>= this.lockDelay)
+        if (this.lockTime >= this.lockDelay)
         {
             Lock();
         }
     }
+
     private void Lock()
     {
         this.board.Set(this);
         this.board.ClearLines();
         this.board.SpawnPiece();
     }
+
     private void HardDrop()
     {
         while (Move(Vector2Int.down))
         {
             continue;
         }
+
         Lock();
     }
+
     private bool Move(Vector2Int translation)
     {
         Vector3Int newPosition = this.position;
         newPosition.x += translation.x;
         newPosition.y += translation.y;
-        bool valid = board.IsValidPosition(this,newPosition);
+        bool valid = board.IsValidPosition(this, newPosition);
         if (valid)
         {
             this.position = newPosition;
             this.moveTime = Time.time + moveDelay;
             this.lockTime = 0f;
         }
+
         return valid;
     }
+
     private void Rotate(int direction)
     {
         int originalRotation = this.rotationIndex;
@@ -125,6 +195,7 @@ public class Piece : MonoBehaviour
             ApplyRotationMatrix(-direction);
         }
     }
+
     private void ApplyRotationMatrix(int direction)
     {
         for (int i = 0; i < this.data.cells.Length; i++)
@@ -137,21 +208,26 @@ public class Piece : MonoBehaviour
                 case Tetromino.O:
                     cell.x -= 0.5f;
                     cell.y -= 0.5f;
-                    x = Mathf.CeilToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
-                    y = Mathf.CeilToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    x = Mathf.CeilToInt((cell.x * Data.RotationMatrix[0] * direction) +
+                                        (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.CeilToInt((cell.x * Data.RotationMatrix[2] * direction) +
+                                        (cell.y * Data.RotationMatrix[3] * direction));
                     break;
                 default:
-                    x = Mathf.RoundToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
-                    y = Mathf.RoundToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    x = Mathf.RoundToInt((cell.x * Data.RotationMatrix[0] * direction) +
+                                         (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.RoundToInt((cell.x * Data.RotationMatrix[2] * direction) +
+                                         (cell.y * Data.RotationMatrix[3] * direction));
                     break;
-
             }
+
             this.cells[i] = new Vector3Int(x, y, 0);
         }
     }
-    private int Wrap(int input, int min, int max)   
+
+    private int Wrap(int input, int min, int max)
     {
-        if (input<min)
+        if (input < min)
         {
             return max - (min - input) % (max - min);
         }
@@ -160,6 +236,7 @@ public class Piece : MonoBehaviour
             return min + (input - min) % (max - min);
         }
     }
+
     private bool TestWallKicks(int rotationIndex, int rotationDirection)
     {
         int wallKickIndex = GetWallKickIndex(rotationIndex, rotationDirection);
@@ -171,8 +248,10 @@ public class Piece : MonoBehaviour
                 return true;
             }
         }
+
         return false;
     }
+
     private int GetWallKickIndex(int rotationIndex, int rotationDirection)
     {
         int wallKickIndex = rotationIndex * 2;
@@ -180,6 +259,7 @@ public class Piece : MonoBehaviour
         {
             wallKickIndex--;
         }
-        return Wrap(wallKickIndex,0,this.data.wallkicks.GetLength(0));
+
+        return Wrap(wallKickIndex, 0, this.data.wallkicks.GetLength(0));
     }
 }
